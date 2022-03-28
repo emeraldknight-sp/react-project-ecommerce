b7855e5108d62952906f079dcdce9839cd432c61
David Almeida <contato.davidalmeida@outlook.com>
2022-03-28 17:49:12 -0300

    ✅ Update: New button styling

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { BooksOnVitrineContext } from "../../providers/BooksOnVitrine";
 import { ProductsOnCartContext } from "../../providers/BookOnCart/index";
 import { StyledButton } from "./styles";
+import { BsCartPlus, BsCartX } from "react-icons/bs";
 
 const Button = ({ type, item }) => {
   const { books, addToBooksOnVitrine, removeFromBooksOnVitrine } = useContext(
@@ -12,13 +13,13 @@ const Button = ({ type, item }) => {
     ProductsOnCartContext
   );
 
-  const text = type === "vitrine" ? "Comprar" : "Remover";
+  const text = type === "vitrine" ? "Adicionar" : "Remover";
 
   const handleClick = () => {
     type === "vitrine" ? addProductOnCart(item) : removeProductFromCart(item);
   };
 
-  return <StyledButton color={text} onClick={handleClick}>{text}</StyledButton>;
+  return <StyledButton color={text} onClick={handleClick}> { text === "Adicionar" ? <BsCartPlus /> : <BsCartX /> } { text}</StyledButton>;
 };
 
 export default Button;
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,19 +1,29 @@
 import styled from "styled-components"
 
 export const StyledButton = styled.button`
-  background-color: ${(props) => props.color === "Comprar" ? "green" : "red" };
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.color === "Adicionar" ? "var(--background-btn)" : "var(--background-btn--negative)" };
   font-size: 20px;
   height: 50px;
   color: white;
   border: none;
   border-radius: 5px;
-  padding: 10px;
+  padding: 5px 10px;
   cursor: pointer;
 
-  transition: filter 0.5s;
+  transition: background-color 0.5s, filter 0.5s;
 
   :active {
     filter: brightness(0.6);
+  }
+
+  @media (min-width: 1024px) {
+    :hover {
+      background-color: var(--gray-3);
+    }
   }
 `
 
