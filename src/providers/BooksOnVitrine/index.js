24ab531fb659fbc13f9d88ce911fc9b6e8953b05
David Almeida <contato.davidalmeida@outlook.com>
2022-03-28 17:52:18 -0300

    ✅ Update: Replacing books from the shelf/showcase

--- src/providers/BooksOnVitrine/index.js
+++ src/providers/BooksOnVitrine/index.js
@@ -6,11 +6,42 @@ export const BooksOnVitrineContext = createContext([])
 export const BooksOnVitrineProvider = ({ children }) => {
 
   const [ books, setBooks ] = useState([
-    { name: "The Witcher - Box", price: 280, image: "https://images-na.ssl-images-amazon.com/images/I/61gUoY6cokL.jpg"},
-    { name: "O Senhor dos Anéis - Box", price: 120, image: "https://images-na.ssl-images-amazon.com/images/I/81CIVene1RL.jpg"},
-    { name: "Sherlock Holmes - Box", price: 70, image: "https://images-na.ssl-images-amazon.com/images/I/714X4gjNs6L.jpg"},
-    { name: "Star Wars: Dark Edition", price: 46, image: "https://images-na.ssl-images-amazon.com/images/I/71sf+k7nCQL.jpg"},
-    { name: "Marvel Guerra Civil - Box", price: 35, image: "https://images-na.ssl-images-amazon.com/images/I/71qMpn5jDWL.jpg"}
+    { 
+      name: "Domain-Driven Design: Atacando as complexidades no coração do software", 
+      author: "Eric Evans",
+      price: 68, 
+      image: "https://images-na.ssl-images-amazon.com/images/I/61aIS4n2jZL._AC_UL160_SR160,160_.jpg"
+    },
+    { 
+      name: "Desenvolvimento Ágil Limpo: De volta às origens", 
+      author: "Robert C. Martin",
+      price: 30, 
+      image: "https://images-na.ssl-images-amazon.com/images/I/6106595rrML._AC_UL160_SR160,160_.jpg"
+    },
+    { 
+      name: "Refatoração: Aperfeiçoando o Design de Códigos Existentes", 
+      price: 99, 
+      author:"Martin Fowler",
+      image: "https://images-na.ssl-images-amazon.com/images/I/81sTm5M7wjL._AC_UL160_SR160,160_.jpg"
+    },
+    { 
+      name: "O Codificador Limpo: Um código de conduta para programadores profissionais",
+      author: "Robert C. Martin",
+      price: 45, 
+      image: "https://images-na.ssl-images-amazon.com/images/I/91lBONZ4tAL._AC_UL160_SR160,160_.jpg"
+    },
+    { 
+      name: "Arquitetura Limpa: O guia do artesão para estrutura e design de software", 
+      author: "Robert C. Martin", 
+      price: 72, 
+      image: "https://images-na.ssl-images-amazon.com/images/I/813bXuhUpQL._AC_UL140_SR140,140_.jpg"
+    },
+    { 
+      name: "Código Limpo: Habilidades práticas do Agile Software", 
+      author: "Robert C. Martin",
+      price: 65, 
+      image: "https://images-na.ssl-images-amazon.com/images/I/71JpZHEGvWL._AC_UL140_SR140,140_.jpg"
+    }
   ])
 
   const addToBooksOnVitrine = (book) => {
